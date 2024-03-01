6c74da0ac64fe5d25c8314836829ed0eb3e245e3
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-03-02 02:48:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import IphoneModel from './components/spline'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
